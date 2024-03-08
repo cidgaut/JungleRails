c9a6e30de2aba0d23ce9abea3b8af148ddac6ca5
Cedric.gauthier <cidgaut@gmail.com>
2024-03-07 22:06:36 -0500

    added authentication parameters and user cookie storage in sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    # previous user and passwords must match
+    if user && user.authenticate(params[:password])
+      #user info is stored in cookie
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # Incorrect login to redirect to new login attempt
+      flash[:alert] = "Email or Password are incorrect."
+      redirect_to '/login'
+    end
   end
 
   def destroy
