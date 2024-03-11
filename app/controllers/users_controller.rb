78252c00c7c0eac3278d3d79b1d585d375a5afdc
Cedric.gauthier <cidgaut@gmail.com>
2024-03-11 13:34:22 -0400

    added rspec to gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,5 +16,13 @@ class UsersController < ApplicationController
         flash[:alert] = "Error creating user."
         redirect_to '/signup'
       end
+    end
   end
+
+  private 
+  
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+  
 end
